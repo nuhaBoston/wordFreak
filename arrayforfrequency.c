7b7f258093d0ec6e6fdaa2bf782f7e09d051b3cf
nuhaBoston <39391126+nuhaBoston@users.noreply.github.com>
2019-03-17 18:52:56 -0400

    yup

--- arrayforfrequency.c
+++ arrayforfrequency.c
@@ -1,3 +1,22 @@
-#include <hashmap.h>
+#include <arrayforfrequency.h>
 
-struct 
+word* Word(char* string, int freq, word *next){
+    word* newWord = (word*)malloc(sizeof(word));
+    if(newWord == NULL){
+        printf("malloc failed\n");
+        exit(EXIT_FAILURE);
+    }    
+    newWord -> string = (char*)malloc(120*sizeof(char));
+    strcpy(newWord -> string, string);
+    newWord -> freq = freq;
+    newWord -> next = next; 
+    return newWord;
+}
+
+char* word_name(word* Word){
+    return Word -> string;
+}
+
+char* createNewWord(word* Word){
+    
+}
