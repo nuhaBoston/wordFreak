386fb09f3886bd3df90a6dfe91633b910dcb94b2
nuhaBoston <39391126+nuhaBoston@users.noreply.github.com>
2019-03-17 17:52:11 -0400

    i dont know what im doing

---
+++ arrayforfrequency.h
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
+/*hashmap to store words and their frequencies */
+
+typedef struct word{
+    char* string;
+    int freq;
+    struct word *next;
+}word;
+
+word* newWord(char* string, int freq, word *next);
+
+
+
+
+
+
--- main.c
+++ main.c
@@ -1,19 +1,23 @@
-#include <stdio.h>
-#include <sys/types.h>
-#include <sys/stat.h>
+
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <errno.h>
-#include <stdlib.h>
 #include <stdio.h>
+
+/*i need to open the txt read it and count the word frequencies and then write another txt to count the frequencies*/
 
 
  int main(int argc, char *argv[]){
 
-     int fd = open(argv[1], O_RDONLY);
+    int fd = open(argv[1],O_RDONLY | O_CREAT);
 
   char article[fd]; 
+
+  if(argc < 2){
+      write(fd, 300, 1000);
+
+  }
 
  if(fd < 0){
     printf("File does not exist!");
